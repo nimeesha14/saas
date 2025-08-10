bc0da5f4fa57aad39e38f0eb3e31a54db44b9dd4
nimeesha14 <nimeeshavaghela@gmail.com>
2025-08-10 22:24:08 +0530

    Initial commit

---
+++ src/home/view.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request):
+    return HttpResponse("Hello World")
+
+def about_view(request):
+    return render(request,'home.html')
