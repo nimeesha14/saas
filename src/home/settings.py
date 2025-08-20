f42ef87dd41006a8a83abdfa3aedaaefb7589f8e
nimeesha14 <nimeeshavaghela@gmail.com>
2025-08-20 16:12:44 +0530

    ldn chnages

--- src/home/settings.py
+++ src/home/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',    
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,6 +132,15 @@ STATIC_ROOT = BASE_DIR.parent / 'local-cdn'
 if not DEBUG:
     STATIC_ROOT = BASE_DIR / 'prod-cdn'
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+STORAGES = {
+    
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
